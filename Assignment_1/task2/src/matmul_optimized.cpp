de5429a8a2497c478e6989c07170aab07c8f64e3
mukunth-kart <mukunthprathishs@gmail.com>
2026-09-06 23:56:37 +0530

    Updated Matrix Optimization

--- Assignment_1/task2/src/matmul_optimized.cpp
+++ Assignment_1/task2/src/matmul_optimized.cpp
@@ -11,6 +11,8 @@
 #include <algorithm>
 #include "matmul.h"
 
+#define PD 16
+
 const int T = 32;
 void matmul_optimized(const float* A, const float* B, float* C,
                       int M, int N, int K, int lda, int ldb, int ldc) {
@@ -23,17 +25,39 @@ void matmul_optimized(const float* A, const float* B, float* C,
             for(int k=0; k<K; k+=T)
             {
                 // TxT mini matrix multiplication
-                for(int i1=i; i1<i+T; i1++)
+                for(int i1=i; i1<i+T; i1+=PD)
                 {
-                    for(int j1=j; j1<j+T; j1++)
+
+                    _mm_prefetch(&A[static_cast<long>(i1) * lda], _MM_HINT_T0);
+                    for(int i2=i1; i2<i1+PD; i2++)
                     {
-                        float acc = 0.0f;
+
+                    for(int j1=j; j1<j+T; j1+=PD)
+                    {
+                        
+                        
                         // const float* a = A + static_cast<long>(i1) * lda;
                         // const float* b = B + static_cast<long>(j1) * ldb;
-                        for (int p = k; p < k+T; p++) {
-                            acc += A[static_cast<long>(i1) * lda + p] * B[static_cast<long>(j1) * ldb + p];
+                        _mm_prefetch(&B[static_cast<long>(j1) * ldb], _MM_HINT_T0);
+                        for(int j2=j1; j2<j1+PD; j2++)
+                        {
+                            __m256 acc = _mm256_setzero_ps();
+                            for (int k1 = k; k1 < k+T; k1+=8) {
+                                __m256 a = _mm256_load_ps(&A[static_cast<long>(i2) * lda + k1]);
+                                __m256 b = _mm256_load_ps(&B[static_cast<long>(j2) * ldb + k1]);
+
+                                acc = _mm256_fmadd_ps(a, b, acc); //A[static_cast<long>(i1) * lda + p] * B[static_cast<long>(j1) * ldb + p];
+                            }
+                            float temp[8];
+
+                            _mm256_store_ps(temp, acc);
+
+                            float total_sum = temp[0] + temp[1] + temp[2] + temp[3] + 
+                                            temp[4] + temp[5] + temp[6] + temp[7];
+
+                            C[static_cast<long>(i2) * ldc + j2] += total_sum;
                         }
-                        C[static_cast<long>(i1) * ldc + j1] += acc;
+                    }
                     }
                 }
             }
--- Assignment_1/task2/src/matmul_prefetch.cpp
+++ Assignment_1/task2/src/matmul_prefetch.cpp
@@ -3,61 +3,31 @@
 #include <immintrin.h>
 #include <algorithm>
 #include "matmul.h"
-#define PD 32
 
-int T = 128;
+#define PREFETCH_DIST 256
 
 void matmul_prefetch(const float* A, const float* B, float* C,
                      int M, int N, int K, int lda, int ldb, int ldc) 
 {
     // TODO(student): replace this placeholder with your cache-blocked SIMD + prefetch
     // implementation.
-    std::fill(C, C + (M * N), 0.0f);
-    for(int i=0; i<M; i+=T)
-    {
-        for(int j=0; j<N; j+=T)
-        {
-            for(int k=0; k<K; k+=T)
-            {
-                // TxT mini matrix multiplication
-                for(int i1=i; i1<i+T; i1+=PD)
-                {
+    for (int i = 0; i < M; i++) {
+        
+        for (int j = 0; j < N; j++) {
+            
+            float acc = 0.0f;
+            const float* a = A + static_cast<long>(i) * lda;
+            const float* b = B + static_cast<long>(j) * ldb;
+            for (int p = 0; p < K; ++p) {
 
-                    _mm_prefetch(&A[static_cast<long>(i1) * lda], _MM_HINT_T0);
-                    for(int i2=i1; i2<i1+PD; i2++)
-                    {
+                _mm_prefetch(reinterpret_cast<const char*>(&a[p + PREFETCH_DIST]), _MM_HINT_T0);
+                _mm_prefetch(reinterpret_cast<const char*>(&b[p + PREFETCH_DIST]), _MM_HINT_T0);
 
-                    for(int j1=j; j1<j+T; j1+=PD)
-                    {
-                        
-                        
-                        // const float* a = A + static_cast<long>(i1) * lda;
-                        // const float* b = B + static_cast<long>(j1) * ldb;
-                        _mm_prefetch(&B[static_cast<long>(j1) * ldb], _MM_HINT_T0);
-                        for(int j2=j1; j2<j1+PD; j2++)
-                        {
-                            __m256 acc = _mm256_setzero_ps();
-                            for (int k1 = k; k1 < k+T; k1+=8) {
-                                __m256 a = _mm256_load_ps(&A[static_cast<long>(i2) * lda + k1]);
-                                __m256 b = _mm256_load_ps(&B[static_cast<long>(j2) * ldb + k1]);
-
-                                acc = _mm256_fmadd_ps(a, b, acc); //A[static_cast<long>(i1) * lda + p] * B[static_cast<long>(j1) * ldb + p];
-                                
-                            }
-                            float temp[8];
-
-                            _mm256_store_ps(temp, acc);
-
-                            float total_sum = temp[0] + temp[1] + temp[2] + temp[3] + 
-                                            temp[4] + temp[5] + temp[6] + temp[7];
-
-                            C[static_cast<long>(i2) * ldc + j2] += total_sum;
-                        }
-                    }
-                    }
-                }
+                acc += a[p] * b[p];
             }
+            C[static_cast<long>(i) * ldc + j] = acc;
+        
         }
     }
-    //matmul_naive(A, B, C, M, N, K, lda, ldb, ldc);
+    // matmul_naive(A, B, C, M, N, K, lda, ldb, ldc);
 }
