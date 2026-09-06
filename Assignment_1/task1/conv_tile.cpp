75716cafc516a63539cf07b5f471003be08ad98c
P-Pramod-Chandra <pramodchandra.p2@gmail.com>
2026-09-06 15:34:32 +0530

    included bound checks for tiling to omit seg faults

--- Assignment_1/task1/conv_tile.cpp
+++ Assignment_1/task1/conv_tile.cpp
@@ -1,4 +1,4 @@
-// // conv_tile.cpp  STAGE 3: CACHE TILING
+// conv_tile.cpp  STAGE 3: CACHE TILING
 
 #include "convolution.h"
 
@@ -7,12 +7,14 @@ void conv_tile(const float* in, float* out, const float* ker,
     // TODO(student): replace this placeholder with your tiled/blocked implementation.
     const int p = K/2;
     const int in_stride = W+2*p; //padded row stride
-    const int B=256;
+    const int B=1024;
     int oy=0,ox=0;    
     for(oy=0;oy<H;oy+=B){
     	for(ox=0;ox<W;ox+=B){
-    		 for (int y = oy; y < oy + B ; y++) {
-                for (int x = ox; x < ox + B; x++) {
+            const int y_end = oy + B > H? H : oy + B;
+            const int x_end = ox + B > H? H : ox + B;
+    		for (int y = oy; y < y_end  ; y++) {
+                for (int x = ox; x < x_end ; x++) {
                     float acc = 0.0f;
                     for (int ky = 0; ky < K; ky++) {
                         for (int kx = 0; kx < K; kx++) {
@@ -25,5 +27,4 @@ void conv_tile(const float* in, float* out, const float* ker,
     		}
     	}
     }
-
 }
